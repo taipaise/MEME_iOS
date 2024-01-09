544a9ae55fb679117b9f540dd6fb5efb334c3285
taipaise <taipaise@gmail.com>
2024-01-09 13:21:00 +0900

    project 생성

---
+++ MEME/MEME/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MEME
+//
+//  Created by 이동현 on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
