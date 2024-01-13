dd72082a29a089c8e40b03933dcbd6823181b494
woongaaaa <woonge0930@naver.com>
2024-01-13 18:49:19 +0900

    ArtistReservationStatusTableViewCell xib, swift 파일 추가 및 아티스트 홈 화면 UI  작성 중...

--- MEME/MEME/Artist/ArtistHomeViewController.swift
+++ MEME/MEME/Artist/ArtistHomeViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ArtistHomeViewController: UIViewController {
-
+    @IBOutlet var artistHomeProfileStatusView: UIView!
+    @IBOutlet var artistProfileImageView: UIImageView!
+    @IBOutlet var ArtistHomeProfileLabel: UILabel!
+    
+    @IBOutlet var onComingButton: UIButton!
+    @IBOutlet var completeButton: UIButton!
+    @IBOutlet var artistReservationStatusTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        onComingButton.configuration?.imagePadding = CGFloat(8)
+        completeButton.configuration?.imagePadding = CGFloat(8)
+        
+        ArtistHomeProfileLabel.text = "안녕하세요, 00님!\n내일 예약 1건이 있어요."
+        ArtistHomeProfileLabel.font = UIFont(name: "Bold", size: 25)
+        ArtistHomeProfileLabel.textAlignment = .left
+        
     }
 
 }
---
+++ MEME/MEME/Artist/ArtistReservationStatusTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArtistReservationStatusTableViewCell.swift
+//  MEME
+//
+//  Created by 황채웅 on 1/13/24.
+//
+
+import UIKit
+
+class ArtistReservationStatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
