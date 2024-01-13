b2c41f6730f77e39f594d12dadfccde61cdcf3b7
taipaise <taipaise@gmail.com>
2024-01-13 23:13:50 +0900

    [Design] 프로필 이미지 등록 VC UI 구현

---
+++ MEME/MEME/Login/SetProfile/SetProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SetProfileViewController.swift
+//  MEME
+//
+//  Created by 이동현 on 1/13/24.
+//
+
+import UIKit
+
+final class SetProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageVIew: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+    @IBAction func imageSelectButtonTapped(_ sender: Any) {
+    }
+    
+}
