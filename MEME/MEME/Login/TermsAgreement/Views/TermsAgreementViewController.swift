d98733454476702185ed205f912de0ec04250e35
taipaise <taipaise@gmail.com>
2024-01-13 21:57:26 +0900

    [Design] 약관 동의 화면 UI구현

---
+++ MEME/MEME/Login/TermsAgreement/Views/TermsAgreementViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TermsAgreementViewController.swift
+//  MEME
+//
+//  Created by 이동현 on 1/13/24.
+//
+
+import UIKit
+
+class TermsAgreementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
