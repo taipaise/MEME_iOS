77bac5388d76ac6270b1df879116b00c2c5358b7
woongaaaa <78294459+woongaaaa@users.noreply.github.com>
2024-01-22 01:41:56 +0900

    Update ArtistReservationSingleManageViewController.swift
    메서드 private 적용

--- MEME/MEME/Artist/ArtistReservationSingleManageViewController.swift
+++ MEME/MEME/Artist/ArtistReservationSingleManageViewController.swift
@@ -18,10 +18,10 @@ class ArtistReservationSingleManageViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func backBtnDidTap(_ sender: UIButton) {
+    @IBAction private func backBtnDidTap(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
-    @IBAction func reservationCancelBtnDidTap(_ sender: UIButton) {
+    @IBAction private func reservationCancelBtnDidTap(_ sender: UIButton) {
         let alert = UIAlertController(title: "예약 취소하기", message: "\n예약을 취소하시겠습니까? 취소 시 모델에게 취소 알림이 전송됩니다.", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "예", style: .default, handler : nil )
         let noAction = UIAlertAction(title: "아니오", style: .cancel, handler : nil )
