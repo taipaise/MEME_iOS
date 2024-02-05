4042183b19c6d4ef7346c042232a833c931dc36c
taipaise <taipaise@gmail.com>
2024-02-05 20:33:20 +0900

    [Move] 파일 이동

---
+++ MEME/MEME/Core/Model/DayOfWeek.swift
@@ -0,0 +1,18 @@
+//
+//  DayOfWeek.swift
+//  MEME
+//
+//  Created by 이동현 on 2/5/24.
+//
+
+import Foundation
+
+enum DayOfWeek: String {
+    case MON
+    case TUE
+    case WED
+    case THU
+    case FRI
+    case SAT
+    case SUN
+}
---
+++ MEME/MEME/Core/Model/ReservationState.swift
@@ -0,0 +1,14 @@
+//
+//  ReservationState.swift
+//  MEME
+//
+//  Created by 이동현 on 2/5/24.
+//
+
+import Foundation
+
+enum ReservationState: String {
+    case EXPECTED
+    case COMPLETE
+    case CANCEL
+}
