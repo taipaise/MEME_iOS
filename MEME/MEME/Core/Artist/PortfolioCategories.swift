a7bcf4bfa2a3e604d2e2e764d8b10b2506a81e01
woongaaaa <woonge0930@naver.com>
2024-03-08 19:47:39 +0900

    Artist 폴더 정리 및 아티스트 API 구현

---
+++ MEME/MEME/Core/Artist/PortfolioCategories.swift
@@ -0,0 +1,40 @@
+//
+//  PortfolioCategories.swift
+//  MEME
+//
+//  Created by 황채웅 on 2/15/24.
+//
+
+import Foundation
+enum PortfolioCategories: String, Codable, CaseIterable {
+    case DAILY
+    case INTERVIEW
+    case ACTOR
+    case PARTY
+    case WEDDING
+    case PROSTHETIC
+    case STUDIO
+    case ETC
+    
+    var korName: String {
+        switch self {
+        case .DAILY:
+            return "데일리 메이크업"
+        case .INTERVIEW:
+            return "배우 메이크업"
+        case .ACTOR:
+            return "면접 메이크업"
+        case .PARTY:
+            return "파티/이벤트 메이크업"
+        case .WEDDING:
+            return "웨딩 메이크업"
+        case .PROSTHETIC:
+            return "특수 메이크업"
+        case .STUDIO:
+            return "스튜디오 메이크업"
+        case .ETC:
+            return "기타(속눈썹, 퍼스널컬러)"
+        }
+    }
+}
+
--- MEME/MEME/Network/Response/PortfolioDTO.swift
+++ MEME/MEME/Network/Response/PortfolioDTO.swift
@@ -7,6 +7,52 @@
 
 import Foundation
 
+struct CreatePortfolioDTO: Codable {
+    let statusCode: Int
+    let result: String
+    let message: String
+    let data: Int
+}
+
+struct GetAllPortfolioDTO: Codable {
+    let result: String
+    let message: String
+    let data: PortfolioData?
+    let statusCode: Int
+}
+struct PortfolioAllDTO: Codable {
+    let content: [PortfolioAllData]?
+    let currentPage: Int
+    let pageSize: Int
+    let totalNumber: Int?
+    let totalPage: Int
+}
+struct EditPortfolioDTO: Codable {
+    struct Response: Codable {
+        let code: Int
+        let result: String
+        let message: String
+        let data: PortfolioData?
+    }
+}
+struct PortfolioAllData: Codable {
+    let portfolioId: Int
+    let category, artistNickName: String
+    let userId: Int
+    let makeupName: String
+    let price: Int
+    let makeupLocation, shopLocation: String
+    let region: [String]
+    let isBlock: Bool
+    let averageStars: String
+    let reviewCount: Int
+    let portfolioImgDtoList: [PortfolioImgDtoList]
+}
+struct PortfolioImgDtoList: Codable {
+    let portfolioImgId: Int
+    let portfolioImgSrc: String
+    let delete: Bool
+}
 // MARK: -아티스트 추천 DTO
 struct RecommendDTO: Codable {
     let result: String
