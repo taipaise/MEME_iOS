e710e0ca3a7ea19564637405fa26dc339d2b276f
taipaise <taipaise@gmail.com>
2024-03-28 18:59:38 +0900

    [Fix] 네비게이션 바 중복 문제 해결

--- MEME/MEME/Core/TabBar/ModelTabBarController.swift
+++ MEME/MEME/Core/TabBar/ModelTabBarController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ModelTabBarController: UITabBarController {
     
-    private var tabs: [TabBarItemType: UINavigationController] = [:]
+    private var tabs: [TabBarItemType: UIViewController] = [:]
 //    private var tabs: [TabBarItemType: UIViewController] = [:]
 
     
@@ -21,29 +21,19 @@ final class ModelTabBarController: UITabBarController {
     
     private func setTabBarItems() {
         tabs = [
-            TabBarItemType.modelHome: UINavigationController(rootViewController: ModelHomeViewController()),
-            TabBarItemType.modelReservation: UINavigationController(rootViewController: ModelReservationChartViewController()),
-            TabBarItemType.modelMypage: UINavigationController(rootViewController: ModelMyPageViewController())
+            TabBarItemType.modelHome: ModelHomeViewController(),
+            TabBarItemType.modelReservation: ModelReservationChartViewController(),
+            TabBarItemType.modelMypage: ModelMyPageViewController()
         ]
-//        private func setTabBarItems() {
-//                tabs = [
-//                    TabBarItemType.modelHome: ModelHomeViewController(),
-//                    TabBarItemType.modelReservation: ModelReservationViewController(),
-//                    TabBarItemType.modelMypage: ModelMyPageViewController()
-//                ]
-//        
-            tabs.forEach { tabType, navigationController in
-                navigationController.tabBarItem = tabType.setTabBarItem()
-            }
-//            tabs.forEach { tabType, viewController in
-//                viewController.tabBarItem = tabType.setTabBarItem()
-//            }
+        
+        tabs.forEach { tabType, navigationController in
+            navigationController.tabBarItem = tabType.setTabBarItem()
+        }
+        
         let sortedKeys = tabs.sorted { $0.key.rawValue < $1.key.rawValue }
         
         let tabNavigationControllers = sortedKeys.map { tabs[$0.key] ?? UINavigationController() }
         setViewControllers(tabNavigationControllers, animated: false)
-//        let tabNavigationControllers = sortedKeys.map { tabs[$0.key] ?? UIViewController() }
-//        setViewControllers(tabNavigationControllers, animated: false)
     }
     
     private func setUI() {
