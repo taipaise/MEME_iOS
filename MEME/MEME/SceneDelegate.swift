67d5c52d756883ad1a4a3f905834c4894fdc64f6
ekrud99 <dagyeong33@gmail.com>
2024-04-30 00:25:15 +0900

    [Feat] 다크모드 전체 비활성화

--- MEME/MEME/SceneDelegate.swift
+++ MEME/MEME/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = getRootViewController()
         window.makeKeyAndVisible()
+        window.overrideUserInterfaceStyle = .light
         self.window = window
     }
 
